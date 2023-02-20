14f96689835f16e97c0998a11fed751f56dc8dca
Mariano Quiroga Ramírez <marianoquirogasigloxxi@gmail.com>
2023-02-20 16:59:41 -0300

    footer responsive

--- practico2/src/componets/footer/Footer.jsx
+++ practico2/src/componets/footer/Footer.jsx
@@ -18,10 +18,10 @@ const Footer = () => {
     return(
         <footer>
             <div className='divpadrefooter'>
-                <div>
+                <div className='divform'>
                     <p>Con gusto nos comunicaremos contigo, déjanos tu email y suscríbete a nuestro Newsletter</p>
-                    <form ref={form} onSubmit={sendEmail} className='mail' action="">
-                        <button type="submit" value="Send" className='enviarboton'><i class="bi bi-send-fill"></i></button>
+                    <form ref={form} onSubmit={sendEmail} className='mail footermail' action="">
+                        <button type="submit" value="Send"><i class="bi bi-send-fill"></i></button>
                         <input type="email" name='user_email' placeholder='Ingresa tu email...'/>
                     </form>
                 </div>
